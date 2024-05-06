49a31663be50fa9f4c57d4656d44ee1a0bb2e95b
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-05-06 20:44:23 +0100

    Footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "../images/logo.png"
+
 
 export default function Footer() {
   return (
     <div>
-      <section class="footer">
+      {/* <section class="footer">
         <h4>About Us</h4>
         <p>
           The Learning Institute offers top-tier online tuition with qualified
@@ -31,7 +33,46 @@ export default function Footer() {
           <p>@CopyRight 2024</p>
         </div>
 
-      </section> 
+      </section>  */}
+
+<footer>
+        
+
+
+        <div class="footerContent">
+
+            <div class="quickLinks">
+                <p class="footerHeadings">Quick Links</p>
+                <p class="footerText">Home</p>
+                <p class="footerText">About Us</p>
+                <p class="footerText">Services</p>
+                <p class="footerText">Prices</p>
+                <p class="footerText">Contact Us</p>
+            </div>
+
+            <div class="legalServices">
+                <p class="footerHeadings">Legal Stuff</p>
+                <p class="footerText">Privacy Policy</p>
+                <p class="footerText">Terms & Conditions</p>
+                <p class="footerText">Whistle Blowing Policy</p>
+                <p class="footerText">Other Policies</p>
+            </div>
+
+            <div class="ourServices">
+                <p class="footerHeadings">Contact Info</p>
+                <p class="footerText">Address: London, United Kingdom</p>
+                <p class="footerText">Phone: +44(0)2034880982 </p>
+                <p class="footerText">Email: Info@learning-institute.co.uk</p>
+ 
+            </div>
+
+
+        </div>
+    </footer>
+
+    <div class="copyrightSection">
+        <p> @Copyrights 2024 | All Rights Reserved | The Learning Institute</p>
+    </div>
       
     </div>
   );
