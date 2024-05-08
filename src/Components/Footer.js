5e93dbd8d40a41625e7b868549d7a4f8fcd48157
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-05-08 13:27:08 +0100

    Subscribe button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -35,45 +35,50 @@ export default function Footer() {
 
       </section>  */}
 
-<footer>
-        
+      <footer>
+
 
 
         <div class="footerContent">
 
-            <div class="quickLinks">
-                <p class="footerHeadings">Quick Links</p>
-                <p class="footerText">Home</p>
-                <p class="footerText">About</p>
-                <p class="footerText">Services</p>
-                <p class="footerText">Prices</p>
-                <p class="footerText">Contact</p>
-            </div>
+          <div class="quickLinks">
+            <p class="footerHeadings">Quick Links</p>
+            <p class="footerText">Home</p>
+            <p class="footerText">About</p>
+            <p class="footerText">Services</p>
+            <p class="footerText">Prices</p>
+            <p class="footerText">Contact</p>
+          </div>
 
-            <div class="legalServices">
-                <p class="footerHeadings">Legal Stuff</p>
-                <p class="footerText">Privacy Policy</p>
-                <p class="footerText">Terms & Conditions</p>
-                <p class="footerText">Whistle Blowing Policy</p>
-                <p class="footerText">Other Policies</p>
-            </div>
+          <div class="legalServices">
+            <p class="footerHeadings">Legal Stuff</p>
+            <p class="footerText">Privacy Policy</p>
+            <p class="footerText">Terms & Conditions</p>
+            <p class="footerText">Whistle Blowing Policy</p>
+            <p class="footerText">Other Policies</p>
+          </div>
 
-            <div class="ourServices">
-                <p class="footerHeadings">Contact Info</p>
-                <p class="footerText">Address: London, United Kingdom</p>
-                <p class="footerText">Phone: +44(0)2034880982 </p>
-                <p class="footerText">Email: Info@learning-institute.co.uk</p>
- 
-            </div>
+          <div class="ourServices">
+            <p class="footerHeadings">Contact Info</p>
+            <p class="footerText">Address: London, United Kingdom</p>
+            <p class="footerText">Phone: +44(0)2034880982 </p>
+            <p class="footerText">Email: Info@learning-institute.co.uk</p>
+          </div>
+
+          <div class="subscribe-form">
+          <p class="footerHeadings">Newsletter</p>
+            <input type="email" class="email-input" placeholder="Enter your email"/>
+            <button class="subscribe-btn">Subscribe</button>
+          </div>
 
 
         </div>
-    </footer>
+      </footer>
 
-    <div class="copyrightSection">
+      <div class="copyrightSection">
         <p> @Copyrights 2024 | All Rights Reserved | The Learning Institute</p>
-    </div>
-      
+      </div>
+
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,12 +11,11 @@ export default function Navbar() {
         <div class="nav-links" id="navLinks">
           <i class="fa-solid fa-xmark"></i>
           <ul>
-            <li><a href="index.html">HOME</a></li>
+            <li><a href="index.html">Home</a></li>
             <li><a href="index.html">About</a></li>
             <li><a href="index.html">Services</a></li>
             <li><a href="index.html">Prices</a></li>
             <li><a href="index.html">Contact</a></li>
-            <li><a className='phonenumber' tel="+442034880982">+44(0)2034880982</a></li>
           </ul>
         </div>
         <i class="fa-solid fa-bars"></i>
