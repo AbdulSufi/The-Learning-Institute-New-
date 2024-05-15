250ca8cd05ca4a88c7b204629d7e62f69245bb57
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-05-16 00:45:18 +0100

    Responsive update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,36 +3,7 @@ import logo from "../images/logo.png";
 
 export default function Footer() {
   return (
-    <div>
-      {/* <section class="footer">
-        <h4>About Us</h4>
-        <p>
-          The Learning Institute offers top-tier online tuition with qualified
-          teachers for key stages 1 to 4. Our tutors undergo rigorous compliance
-          checks, and we provide a wealth of curriculum-aligned resources. We
-          offer flexible scheduling, manage administrative tasks, and focus on
-          holistic student progress with regular sessional reports.
-        </p>
-
-        <div class="icons">
-          <i class="fa-brands fa-facebook"></i>
-          <i class="fa-brands fa-twitter"></i>
-          <i class="fa-brands fa-instagram"></i>
-          <i class="fa-brands fa-linkedin"></i>
-        </div>
-
-        <div className="contact">
-          <h4>Contact:</h4>
-          <a className='contactDetails' tel="+442034880982"> Tel: +44(0)2034880982</a>
-          <a className='contactDetails' email="Info@learning-institute.co.uk"> Email: Info@learning-institute.co.uk</a>
-          <a className='contactDetails' Address="Info@learning-institute.co.uk"> Address: London, United Kingdom</a>
-        </div>
-
-        <div className="copyright">
-          <p>@CopyRight 2024</p>
-        </div>
-
-      </section>  */}
+    <div className="footer-parent">
 
       <footer>
         <div class="footerContent">
