ac54c8c9110db577c63f9839030747970fe3251e
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-06-04 21:23:26 +0100

    Updates

--- src/Components/Prices.js
+++ src/Components/Prices.js
@@ -1,14 +1,14 @@
 import React from "react";
 import serviceTable from "../images/servicesTableNew.jpg";
-export default Prices; 
+export default Prices;
 
-function Prices(){
+function Prices() {
     return (
         <div className="pricesSection">
-            <h1>Services & Prices</h1>
+            <h1> Prices</h1>
 
             <div className="pricesImage">
-                <img src="serviceTable"/>
+                <img src={serviceTable} />
             </div>
 
         </div>
