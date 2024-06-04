dfea66d4ee0bdaa7d26168d24b437672447cd080
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-06-04 18:42:34 +0100

    Update navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from "../images/LogoNew.png"
 
 export default function Navbar() {
+  var navLinks = ""
+
+  useEffect(()=>{
+    navLinks = document.getElementById("navLinks");
+  }, [])
+        
+  function showMenu(){
+      navLinks.style.right = "0";
+  }
+  function hideMenu(){
+      navLinks.style.right = "-200px";
+      
+  }
   return (
     <div>
        <nav>
@@ -9,7 +22,7 @@ export default function Navbar() {
           <img src={logo} alt="logo" />
         </a>
         <div class="nav-links" id="navLinks">
-          <i class="fa-solid fa-xmark"></i>
+          <i class="fa-solid fa-xmark" onClick={hideMenu}></i>
           <ul>
             <li><a href="index.html">Home</a></li>
             <li><a href="index.html">About</a></li>
@@ -18,7 +31,7 @@ export default function Navbar() {
             <li><a href="index.html">Contact</a></li>
           </ul>
         </div>
-        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-bars" onClick={showMenu}></i>
       </nav>
     </div>
   )
