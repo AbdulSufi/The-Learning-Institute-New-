fb89155799e233fe70780ec49b258a80e765e92b
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-06-10 20:27:18 +0100

    Update latests

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import contactImage from "../images/contactImage.jpg";
 
 export default function ContactUs() {
     return (
-        <div>
-            <h1>Contact Us </h1>
-            <p>Phone: 020 34880982 </p>
-            <p>Email: Info@learning-institute.co.uk</p>
-            <p>Address: Learning Institute, Office 6255, 321-323 High Road, Chadwell Heath, London, RM6 6AX, UK </p>
+        <div className='contact-Section'>
+            
+            <h1 className='contact-h1'>Contact Us</h1> 
+            <div className="contact-container">
+
+                <div className="contact-info">
+                    <p><strong>Phone:</strong> 020 34880982</p>
+                    <p><strong>Email:</strong> <a href="mailto:Info@learning-institute.co.uk">Info@learning-institute.co.uk</a></p>
+                    <p><strong>Address:</strong> Learning Institute, Office 6255, 321-323 High Road, Chadwell Heath, London, RM6 6AX, UK</p>
+                </div>
+
+                <div className='contactImage-container'>
+                    <img className='contact-image' src={contactImage}></img>
+                </div>
+
+
+            </div>
         </div>
+
+
     )
 }
--- src/Components/Privacy.js
+++ src/Components/Privacy.js
@@ -4,6 +4,69 @@ export default function Privacy() {
   return (
     <div>
         <h1>Privacy</h1>
+
+        <p>
+
+        Terms and Conditions
+Welcome to The Learning Institute. By using our tutoring services, both online
+and in person, you agree to the following terms and conditions. Please read
+them carefully:
+1. Information Sharing
+The Learning Institute may share relevant information provided during the
+enrollment process with teachers assigned to the student. This ensures a
+personalized and effective educational experience.
+2. Additional Service Fee
+To ensure comprehensive support and quality service, The Learning
+Institute incorporates an additional 20% fee. This covers essential aspects
+such as planning time, provision of resources, report writing, and assessments.
+This fee reflects our commitment to delivering a thorough educational
+experience with care and consideration.
+3. Recording of Sessions
+All tutoring sessions, both online and in person, will be recorded for
+safeguarding purposes. These recordings are stored securely and
+confidentially in a centralized location for the duration of the tuition cycle.
+Once the tuition cycle is complete, the recordings will be permanently deleted.
+This practice ensures the safety of all parties involved while maintaining strict
+confidentiality.
+4. Cancellation and Punctuality Policy
+Our teachers meticulously arrange their schedules to ensure attendance and
+preparation for each session. We request a minimum of 24 hours&#39; notice for
+cancellations to accommodate changes efficiently. If a cancellation occurs with
+less than 24 hours&#39; notice, a nominal fee equivalent to the full lesson amount
+may be applied. This policy helps us uphold the quality and continuity of our
+educational services while respecting the dedication of our educators.
+5. Refund Policy
+The Learning Institute is dedicated to delivering high-quality tutoring and
+ensuring student and customer satisfaction. If you are dissatisfied with your
+initial tutoring session, you may claim a full refund. This applies only to the
+first session and must be claimed within 24 hours of the session&#39;s conclusion.
+6. Confidentiality and Data Protection (GDPR Compliance)
+We take your privacy seriously and adhere to the General Data Protection
+Regulation (GDPR). All personal information and recorded sessions are
+treated with strict confidentiality and stored securely. Information collected
+will only be used for the purpose of providing tutoring services and will not be
+shared with unauthorized parties. You have the right to access, rectify, or
+
+delete your personal data at any time. For any data protection inquiries, please
+contact our Data Protection Officer at [contact email].
+7. **Commitment to Quality and Service**
+The Learning Institute commits to providing exceptional educational
+services through qualified and experienced tutors. We aim to meet the
+educational needs of each student with a personalized approach, ensuring that
+learning goals are met effectively.
+8. **Contact Information**
+If you have any questions or concerns regarding these terms and conditions
+or our services, please contact us at [contact email] or [contact phone
+number].
+By continuing to use our services, you acknowledge that you have read and
+understood these terms and conditions. Your trust in our commitment to
+quality education is important to us.
+
+
+
+        </p>
+
+    
     </div>
   )
 }
