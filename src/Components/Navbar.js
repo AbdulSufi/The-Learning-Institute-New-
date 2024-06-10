6648d1845580d74121cd7f6280bc2239e3872c57
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-06-11 03:49:54 +0500

    Navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,26 @@
-import React, { useEffect } from 'react'
-import logo from "../images/LogoNew.png"
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import logo from "../images/LogoNew.png";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  var navLinks = ""
-
-  useEffect(() => {
-    navLinks = document.getElementById("navLinks");
-  }, [])
+  const [menuVisible, setMenuVisible] = useState(false);
 
   function showMenu() {
-    navLinks.style.right = "0";
+    setMenuVisible(true);
   }
-  function hideMenu() {
-    navLinks.style.right = "-200px";
 
+  function hideMenu() {
+    setMenuVisible(false);
   }
+
   return (
     <div>
       <nav>
         <Link to={"/"}>
           <img src={logo} alt="logo" />
         </Link>
-        <div class="nav-links" id="navLinks">
-          <i class="fa-solid fa-xmark" onClick={hideMenu}></i>
+        <div className={`nav-links ${menuVisible ? 'visible' : ''}`}>
+          <i className="fa-solid fa-xmark" onClick={hideMenu}></i>
           <ul>
             <li><Link to={"/"} onClick={hideMenu}>Home</Link></li>
             <li><Link to={"/aboutUs"} onClick={hideMenu}>About</Link></li>
@@ -31,8 +28,8 @@ export default function Navbar() {
             <li><Link to={"/contactUs"} onClick={hideMenu}>Contact</Link></li>
           </ul>
         </div>
-        <i class="fa-solid fa-bars" onClick={showMenu}></i>
+        <i className="fa-solid fa-bars" onClick={showMenu}></i>
       </nav>
     </div>
-  )
+  );
 }
