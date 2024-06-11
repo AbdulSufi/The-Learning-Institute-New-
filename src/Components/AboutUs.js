25ff362e106e3ad9ebe1b8b7ed1bc2915a7ba7c3
AbdulSufi <abduwaheb.sufi@gmail.com>
2024-06-11 13:28:29 +0100

    update aboutUs

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -10,10 +10,10 @@ export default function AboutUs() {
     //     <div className='aboutUs-textBox'>
     //       <p className='aboutUs-text'> At the Learning Institute, we're not just tutors; we're a dedicated team of qualified and experienced teachers
     //         committed to shaping the academic journey of primary and secondary school students in London. With a focus on
-    //         personalized tuition, we understand that every student is unique, and so is their learning style.
+    //         personalised tuition, we understand that every student is unique, and so is their learning style.
     //         <br></br> <br></br>
-    //         Our exceptional educators specialize in catering to diverse pupil needs and groups, including those with Special Educational Needs (SEN),
-    //         Looked After Children (LAC), and Alternative Provision (AP). Through tailored support and personalized attention, we empower each student to progress confidently, maximizing their
+    //         Our exceptional educators specialise in catering to diverse pupil needs and groups, including those with Special Educational Needs (SEN),
+    //         Looked After Children (LAC), and Alternative Provision (AP). Through tailored support and personalised attention, we empower each student to progress confidently, maximizing their
     //         potential and achieving academic success.
     //         <br></br> <br></br>
     //         What sets us apart is our holistic approach to education.
